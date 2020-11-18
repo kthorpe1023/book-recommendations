9285ba515c19e95f82975125309c64b9c7d1aedd
Katie Thorpe <kthorpe1023@gmail.com>
2020-11-17 21:33:41 -0700

    book component added

---
+++ client/src/components/Book/Book.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Book.css"
+
+function Book (){
+    return(
+    <div class="container">
+        <img src="../public/logo512.png" alt="Avatar" class="image"></img>
+        <div class="overlay">
+            <div class="text">Hello World</div>
+        </div>
+    </div>
+    )
+}
+
+export default Book;
